1adf1e5357ffb5191000888410fca5bb7e212603
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-02-10 08:35:54 -0800

    add basic paths to app.php, add twig files

--- app/app.php
+++ app/app.php
@@ -9,7 +9,37 @@
     ));
     $app['debug'] = true;
 
+    session_start();
+    if(empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
+
     $app->get('/', function() use ($app) {
-      return "Hello World";
+
+
+        return "/";
+        // return $app['twig']-render('index.html.twig', array('contacts' => Contact::getAll()));
     });
+
+
+    $app->post('/create_contact', function() use ($app) {
+        //After submitting the form, the new Contacts should be saved into the session under the key $_SESSION['list_of_contacts']
+
+        //CALL SAVE()
+        return "/create_contact";
+        // return $app['twig']->render('/show_contact.html.twig', array('list_of_contacts' => $_SESSION['list_of_contacts']));
+    });
+
+
+
+    $app->post("/delete_contacts", function() use ($app) {
+        //Contact::deleteAll();
+
+        return "/delete_contacts";
+        // return $app['twig']->render('delete_contacts.html.twig');
+    });
+
+
+
     return $app;
